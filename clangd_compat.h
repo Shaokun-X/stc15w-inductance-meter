992fd4fc1f07d95228202e5a39ccf8ed6ab34ddd
Shaokun-X <shaokun.xie@outlook.fr>
2026-08-22 19:48:32 +0200

    clangd toolchain support

---
+++ clangd_compat.h
@@ -0,0 +1,18 @@
+#ifndef CLANGD_COMPAT_H
+#define CLANGD_COMPAT_H
+
+/* Approximate SDCC extensions for Clang-based editor tooling only. */
+#ifdef __clang__
+/* Avoid Clang's hosted-program signature requirement for firmware main. */
+#define main sdcc_main
+#define __sfr volatile unsigned char
+#define __sbit volatile unsigned char
+#define __bit unsigned char
+#define __xdata
+#define __code
+#define __at(address)
+#define __interrupt(vector) \
+    __attribute__((diagnose_if((vector) < 0, "invalid interrupt vector", "error")))
+#endif
+
+#endif
